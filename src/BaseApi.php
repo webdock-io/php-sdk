9307deb616f5cb92801de1133db205eff419fbc1
Pujianto <hello@pujianto.net>
2020-10-18 02:04:43 +0700

    add try catch in api calls

--- src/BaseApi.php
+++ src/BaseApi.php
@@ -1,6 +1,9 @@
 <?php
 namespace Webdock;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+use Webdock\Exception\WebdockException;
 use Webdock\WebdockObject;
 
 abstract class BaseApi implements ApiInterface
@@ -14,7 +17,20 @@ abstract class BaseApi implements ApiInterface
 
     protected function execute($endpoint, $method, $params)
     {
-        $response = $this->client->request($method, $endpoint, $params);
+        try {
+            $response = $this->client->request($method, $endpoint, $params);
+        } catch (RequestException $e) {
+            $errors = [];
+            $errors[] = sprintf(
+                'Error %s',
+                $e->getCode()
+            );
+            if ($e->hasResponse()) {
+                $errors[] = $e->getResponse()->getBody()->getContents();
+            }
+            $error = implode("\n", $errors);
+            throw new WebdockException($error, $e->getCode());
+        }
         return new WebdockObject($response);
     }
 }
